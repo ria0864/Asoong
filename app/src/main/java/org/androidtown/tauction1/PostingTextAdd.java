614e45d0d5306ea21097a6cfc4ae944066b46eed
childofsangdo <sjjo0319@naver.com>
2016-08-21 14:37:49 +0900

    필터 레이아웃

--- app/src/main/java/org/androidtown/tauction1/PostingTextAdd.java
+++ app/src/main/java/org/androidtown/tauction1/PostingTextAdd.java
@@ -2,7 +2,9 @@ package org.androidtown.tauction1;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 import android.widget.ArrayAdapter;
+import android.widget.ImageButton;
 import android.widget.Spinner;
 
 /**
@@ -10,10 +12,19 @@ import android.widget.Spinner;
  */
 public class PostingTextAdd extends AppCompatActivity {
 
+    ImageButton goFragment5;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.posting_text);
+
+        goFragment5 = (ImageButton)findViewById(R.id.goFragment5);
+        goFragment5.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
 
         Spinner postSpinner = (Spinner)findViewById(R.id.spinneraddpost);
         ArrayAdapter postAdapter = ArrayAdapter.createFromResource(this, R.array.spinnerPost, android.R.layout.simple_spinner_item);
